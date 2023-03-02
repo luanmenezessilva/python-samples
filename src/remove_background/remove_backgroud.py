640f5d2a615e9f99dfb244704c27821b0fdf537c
Luan Silva <luan.silva@rozac.com.br>
2023-03-02 14:13:13 -0300

    Nova tentativa

--- src/remove_background/remove_backgroud.py
+++ src/remove_background/remove_backgroud.py
@@ -1,10 +1,12 @@
+# Não funcionou
+
 from rembg import remove
 from PIL import Image
 
-# input_path = 'diamante_branco.jpg'
-input_path = 'C:\Rozac\Pessoal\Fontes\python-samples\src\diamante_branco.jpg'
-output_path = 'C:\Rozac\Pessoal\Fontes\python-samples\src\diamante_branco2.jpg'
+input_path = 'C:\\Rozac\\Pessoal\\Fontes\\python-samples\\src\\remove_background\\diamante_branco.jpg'
+output_path = 'C:\\Rozac\\Pessoal\\Fontes\\python-samples\\src\\remove_background\\diamante_branco2.jpeg'
 
-input = Image.open(input_path)
-output = remove(input)
-output.save(output_path)
+
+im = Image.open(input_path)
+rgb_im = im.convert('RGB')
+rgb_im.save(output_path)
